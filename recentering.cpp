01d27fa3a645ba2fa7303e0bc7e7e539586bbebb
Carolina Villalón <cvillalon@unc.edu.ar>
2021-05-17 16:28:02 -0300

    Update particle position in recentering

--- recentering.cpp
+++ recentering.cpp
@@ -1,6 +1,6 @@
 #include "recentering.h"
 
-void recenter(const float xc_fof, const float yc_fof, const float zc_fof, vector <float> x, vector <float> y, vector <float> z, float *xc_rc, float *yc_rc, float *zc_rc, float r_max){
+void recenter(const float xc_fof, const float yc_fof, const float zc_fof, vector <float> &x, vector <float> &y, vector <float> &z, float *xc_rc, float *yc_rc, float *zc_rc, float r_max){
 
     int np = x.size();
 
--- recentering.h
+++ recentering.h
@@ -7,4 +7,4 @@ using namespace std;
 
 //--------------- recentering coordinates---------------
 
-void recenter(const float xc_fof, const float yc_fof, const float zc_fof, vector <float> x, vector <float> y, vector <float> z, float *xc_rc, float *yc_rc, float *zc_rc, float r_max);
+void recenter(const float xc_fof, const float yc_fof, const float zc_fof, vector <float> &x, vector <float> &y, vector <float> &z, float *xc_rc, float *yc_rc, float *zc_rc, float r_max);
