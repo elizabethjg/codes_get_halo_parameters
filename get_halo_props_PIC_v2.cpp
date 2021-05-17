2f5f4b408e944f83b799896552fea849d96ca3db
eli <elizabeth.johana.gonzalez@gmail.com>
2021-05-17 11:37:09 -0300

    update

--- get_halo_props_PIC_v2.cpp
+++ get_halo_props_PIC_v2.cpp
@@ -260,6 +260,12 @@ for (int ihalo = 0; ihalo < 10; ihalo++) {
     
     printf("Halo %d: %d particles\n", ihalo, Npart);
     printf("log(M_fof) = %.1f\n", lm);
+    printf("xc = %.1f\n", xc_fof);
+    printf("yc = %.1f\n", yc_fof);
+    printf("zc = %.1f\n", zc_fof);
+    printf("vxc = %.1f\n", vxc);
+    printf("vyc = %.1f\n", vyc);
+    printf("vzc = %.1f\n", vzc);
 
 
     if(Npart>3000 && Npart < 40000){
