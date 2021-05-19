7348be2b0e4a840380052e2322d55e85709021c1
Dario g <dgrana@unc.edu.ar>
2021-05-19 19:43:16 -0300

    variables in uppercase were renamed in lowercase

--- compute_profile.cpp
+++ compute_profile.cpp
@@ -4,14 +4,14 @@
 
 
 void ro_r(const vector <float> x_part, const vector <float> y_part, const vector <float> z_part,
-        const int NRINGS, const float MAX_DISTANCE, vector <float> ro){
+        const int nrings, const float max_distance, vector <float> ro){
 
-    float V;
-    int NPART = x_part.size();
+    float V; //¿que representa esta variable?
+    int npart = x_part.size();
 
-    const float ring_width = MAX_DISTANCE / NRINGS;
+    const float ring_width = max_distance / nrings;
 
-    for (i = 0; i < NPART; i++){
+    for (i = 0; i < npart; i++){
 
         rsq = pow(x_part[i], 2) + pow(y_part[i], 2) + pow(z_part[i], 2)
 
@@ -20,8 +20,8 @@ void ro_r(const vector <float> x_part, const vector <float> y_part, const vector
         /* Este if hace falta para el caso en que la distancia sea
          * exactamente igual a la máxima distancia posible porque se
          * suma partícula en cada anillo si r1 <= r < r2.*/
-        if (idx >= NRINGS){
-            idx = NRINGS - 1;
+        if (idx >= nrings){
+            idx = nrings - 1;
         }
 
         ro[idx] += 1;
@@ -29,15 +29,15 @@ void ro_r(const vector <float> x_part, const vector <float> y_part, const vector
 
     /* Chequeo del número de partículas por anillo y del total.*/
     int total = 0;
-    for (i = 0; i < NRINGS; i++){
+    for (i = 0; i < nrings; i++){
         total += ro[i];
     }
 
-    if (total != NPART){
-		printf("%d particle(s) are missing\n", NPART - total);
+    if (total != npart){
+		printf("%d particle(s) are missing\n", npart - total);
     }
 
-    for (i = 0; i < NRINGS; i++){
+    for (i = 0; i < nrings; i++){
 
         rin = ring_width * i
         V = (4./3.) * pi * (pow((rin + ring_witdth), 3) - pow(rin, 3))
