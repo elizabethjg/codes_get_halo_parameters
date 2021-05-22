809171790a41c899ced27ca44fcbdc88f9d6c08e
Dario G <dgrana@unc.edu.ar>
2021-05-22 11:46:11 -0300

    minor changes made
    reduction clause added for Ekin_part and Epot_part
    dead comments weree removed

--- halo_energy.cpp
+++ halo_energy.cpp
@@ -13,40 +13,46 @@ void halo_energy(const vector <float> x, const vector <float> y,
     double Mpc = 3.08567758e22; // [m/Mpc]
     double fEkin = 0.5*h*Msun*1.e-40; //for energy unit conversion
     double fEpot = ((Msun*Msun*h*G*1.e-6) / Mpc) * 1.e-40;
-    double Ekin_part, Epot_part;
+    double Ekin_part, Epot_part = 0.;
 
     float xi, yi, zi;
     float dxi, dyi, dzi;
 
     int np = x.size();
 
-    #pragma omp parallel for private(xi,yi,zi,Ekin_part,Epot_part,dxi,dyi,dzi)
+    #pragma omp parallel for \
+        private(xi,yi,zi,dxi,dyi,dzi) \
+        reduction(+:Epot_part,Ekin_part)
     for (int j = 0; j < np; j++) {
 
         xi=x[j]; yi=y[j]; zi=z[j];
 
-        Ekin_part = fEkin*mp*(vx[j]*vx[j] + vy[j]*vy[j] + vz[j]*vz[j]); // in units of (kg*km^2/s^2)*10^40
-
-        Epot_part = 0;
+        // in units of (kg*km^2/s^2)*10^40
+        Ekin_part = fEkin*mp*(vx[j]*vx[j] + vy[j]*vy[j] + vz[j]*vz[j]); 
 
         for (int k = 0; k < np; k++) {
 
             if(k != j){
 
                 // conversion from kpc/h to mpc/h
-                dxi = (xi - x[k]) / 1000.;// dxi = dxi/1000.;
-                dyi = (yi - y[k]) / 1000.;// dyi = dyi/1000.;
-                dzi = (zi - z[k]) / 1000.;// dzi = dzi/1000.;
+                dxi = (xi - x[k]) / 1000.;
+                dyi = (yi - y[k]) / 1000.;
+                dzi = (zi - z[k]) / 1000.;
 
-                Epot_part = Epot_part + fEpot*mp*mp/sqrt(dxi*dxi + dyi*dyi + dzi*dzi);// in units of (kg*m^2/s^2)*10^40
+                // in units of (kg*m^2/s^2)*10^40
+                Epot_part = Epot_part + fEpot*mp*mp/sqrt(dxi*dxi + dyi*dyi + dzi*dzi);
 
             }
 
         }
 
-        *Epot_halo=*Epot_halo + Epot_part;
-        *Ekin_halo=*Ekin_halo + Ekin_part;
+//        *Epot_halo=*Epot_halo + Epot_part;
+//        *Ekin_halo=*Ekin_halo + Ekin_part;
+          Epot_part += Epot_part;
+          Ekin_part += Ekin_part;
 
     }
+    *Epot_halo = Epot_part;
+    *Ekin_halo = Ekin_part;
 }
             //-----------------------------------------------------------------------------------------
