eaebb3fd108926203a8d5b766e56e44172d439c5
eli <elizabeth.johana.gonzalez@gmail.com>
2021-06-01 21:10:20 -0300

    correct ellip and divide mass by h

--- get_halo_props_PIC_v2.cpp
+++ get_halo_props_PIC_v2.cpp
@@ -354,7 +354,7 @@ int main(int argc, char **argv){
 
             // 3D elliptical profile
             vector <double> ro_E = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
-            ro_r(x_part, y_part, z_part, NRINGS, r_max, ro_E, a3D_abs, b3D_abs, c3D_abs);            
+            ro_r(x_rot, y_rot, z_rot, NRINGS, r_max, ro_E, a3D_abs, b3D_abs, c3D_abs);            
             for (int k = 0; k < NRINGS; k++) {
                     outdata_pro <<
                     ro_E[k] << delim; 
@@ -362,7 +362,7 @@ int main(int argc, char **argv){
 
             // 2D profile
             vector <double> Sigma = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
-            Sigma_r(x_part_proj, y_part_proj, NRINGS, r_max, Sigma,1.,1.);
+            Sigma_r(x2d_rot, y2d_rot, NRINGS, r_max, Sigma,1.,1.);
             for (int k = 0; k < NRINGS; k++) {
                     outdata_pro <<
                     Sigma[k] << delim;
