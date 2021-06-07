f131af93479bbd57903b65dacde32b180ee6cf07
eli <elizabeth.johana.gonzalez@gmail.com>
2021-06-07 12:15:03 -0300

    go back

--- compute_profile.cpp
+++ compute_profile.cpp
@@ -29,7 +29,7 @@ void ro_r(const vector <float> x, const vector <float> y, const vector <float> z
         /* Este if hace falta para el caso en que la distancia sea
          * exactamente igual a la máxima distancia posible porque se
          * suma partícula en cada anillo si r1 <= r < r2.*/
-        zif (idx >= nrings){
+        if (idx >= nrings){
             idx = nrings - 1;
         }
 
