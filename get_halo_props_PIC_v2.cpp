09276cb01cf6d87fd6ac00b26afc3f047f1d9f5c
eli <elizabeth.johana.gonzalez@gmail.com>
2021-06-07 12:19:51 -0300

    correct coordinates in elliptical profiles

--- get_halo_props_PIC_v2.cpp
+++ get_halo_props_PIC_v2.cpp
@@ -362,7 +362,7 @@ int main(int argc, char **argv){
 
             // 2D profile
             vector <double> Sigma = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
-            Sigma_r(x2d_rot, y2d_rot, NRINGS, r_max, Sigma,1.,1.);
+            Sigma_r(x_part_proj, y_part_proj, NRINGS, r_max, Sigma,1.,1.);
             for (int k = 0; k < NRINGS; k++) {
                     outdata_pro <<
                     Sigma[k] << delim;
@@ -370,7 +370,7 @@ int main(int argc, char **argv){
             
             // 2D elliptical profile
             vector <double> Sigma_E = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.};
-            Sigma_r(x_part_proj, y_part_proj, NRINGS, r_max, Sigma_E, a2D_abs, b2D_abs);
+            Sigma_r(x2d_rot, y2d_rot, NRINGS, r_max, Sigma_E, a2D_abs, b2D_abs);
             for (int k = 0; k < NRINGS-1; k++) {
                     outdata_pro <<
                     Sigma_E[k] << delim;
