703de9a38b2bd03469d066630380b4aff4c298c4
eli <elizabeth.johana.gonzalez@gmail.com>
2021-07-06 16:28:42 -0300

    compute profile for 10 bins

--- compute_profile.py
+++ compute_profile.py
@@ -2,24 +2,27 @@ import numpy as np
 import pandas as pd
 import argparse 
 
-part0 = np.loadtxt('/mnt/projects/lensing/HALO_SHAPE/MICEv1.0/catalogs/ind_halos/coords5707').T  
+part0 = np.loadtxt('/mnt/projects/lensing/HALO_SHAPE/MICEv1.0/catalogs/ind_halos/coords568416').T  
+# part0 = np.loadtxt('/mnt/projects/lensing/HALO_SHAPE/MICEv1.0/catalogs/ind_halos/coords5707').T  
+
+nrings = 10
 
 xr,yr,zr = part0[3],part0[4],part0[5]
 xp,yp = part0[8],part0[9]
 
 rin = 0
-step = 2398.366/20.
+step = pro[1]/float(nrings)
 
-s = 1.
-q = 1.
-q2 = 1.
+# s = 1.
+# q = 1.
+# q2 = 1.
 
-r_E = np.zeros(20)
-S_E = np.zeros(20)
-rp = np.zeros(20)
+rhop = np.zeros(nrings)
+Sp = np.zeros(nrings)
+rp = np.zeros(nrings)
 
 Ntot = 0
-for ring in range(20):
+for ring in range(nrings):
     
     abin_in = rin/(q*s)**(1./3.)
     bbin_in = abin_in*q
@@ -29,9 +32,9 @@ for ring in range(20):
     bbin_out = abin_out*q
     cbin_out = abin_out*s
     
-    print(abin_out)
+    # print(abin_out)
     
-    rp[ring] = rin + 0.5*step
+    rp[ring] = (rin + 0.5*step)/1.e3
     
     # rpart_E = (abc*(xr**2/abin**2 + yr**2/bbin**2 + zr**2/cbin**2))**(1./3.)/1.e3 
     rpart_E_in = (xr**2/abin_in**2 + yr**2/bbin_in**2 + zr**2/cbin_in**2)
@@ -39,19 +42,22 @@ for ring in range(20):
     
     V    = (4./3.)*np.pi*(((rin+step)/1.e3)**3 - (rin/1.e3)**3)
     mask = (rpart_E_in >= 1)*(rpart_E_out < 1)
-    r_E[ring] = (mask.sum()*mp)/V
+    rhop[ring] = (mask.sum()*mp)/V
 
-    print(mask.sum())
+    # print(mask.sum())
 
     abin_in = rin/np.sqrt(q2) 
-    bbin_in = bbin_in*q2
+    bbin_in = abin_in*q2
+
+    abin_out = (rin+step)/np.sqrt(q2) 
+    bbin_out = abin_out*q2
 
     rpart_E_in = (xp**2/abin_in**2 + yp**2/bbin_in**2)
     rpart_E_out = (xp**2/abin_out**2 + yp**2/bbin_out**2)
     
     A    = np.pi*(((rin+step)/1.e3)**2 - (rin/1.e3)**2)
     mask = (rpart_E_in >= 1)*(rpart_E_out < 1)
-    S_E[ring] = (mask.sum()*mp)/A
+    Sp[ring] = (mask.sum()*mp)/A
 
     rin += step
     
