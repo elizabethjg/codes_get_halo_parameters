799073d189d78736276753bc19508ab136fcda94
eli <elizabeth.johana.gonzalez@gmail.com>
2021-07-21 11:57:42 -0300

    test

--- halo_energy.cpp
+++ halo_energy.cpp
@@ -43,7 +43,7 @@ void halo_energy(const vector <float> x, const vector <float> y,
 
 
                 // in units of (kg*m^2/s^2)*10^40
-                Epot_part += fEpot*mp*mp/sqrt((dxi*dxi + dyi*dyi + dzi*dzi)+pow(50./1000.,2));
+                Epot_part += fEpot*mp*mp/sqrt((dxi*dxi + dyi*dyi + dzi*dzi));
 
             }
 
