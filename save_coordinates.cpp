275d7a396b84177577314c7c20accf2fc7b2225f
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-11-21 12:11:42 -0300

    update

--- save_coordinates.cpp
+++ save_coordinates.cpp
@@ -20,7 +20,7 @@ void save_coordinates(const int ihalo,
 
     //open output file to save profiles
     ofstream outdata_coords;
-    string out_file_coords = "../ind_halos/2_1/coords"+id;
+    string out_file_coords = "../ind_halos/2_2/coords"+id;
     outdata_coords.open(out_file_coords.c_str());
 
     //set format for output
