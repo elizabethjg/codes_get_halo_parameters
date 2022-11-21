56d79911b7d6fd183831f7b835bffb2aa4b77367
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-11-21 11:49:25 -0300

    run save coords 1000 parts

--- save_coordinates.h
+++ save_coordinates.h
@@ -5,6 +5,9 @@
 
 using namespace std;
 
-void save_coordinates(const int ihalo);
-
+void save_coordinates(const int ihalo,
+        const vector <float> x, const vector <float> y, const vector <float> z,
+        const vector <float> x_rot, const vector <float> y_rot, const vector <float> z_rot,
+        const vector <float> x2d, const vector <float> y2d,
+        const vector <float> x2d_rot, const vector <float> y2d_rot);
 #endif
